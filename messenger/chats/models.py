f7bab989d0773f439b029e97ead2912b947acad8
karmanovalexey <karmanov.aa@phystech.edu>
2019-10-29 21:38:20 +0300

    Initial migrations added

--- messenger/chats/models.py
+++ messenger/chats/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Chat(models.Model):
+  is_group_chat = models.BooleanField()
+  topic = models.CharField(max_length=50, null=False)
+  last_message = models.CharField(max_length=50, null=False)
+
+class Message(models.Model):
+  user = models.ForeignKey(User, models.DO_NOTHING)
+  chat = models.ForeignKey(Chat, models.DO_NOTHING)
+  content = models.TextField()
+  added_at = models.DateTimeField()
+
+class Attachment(models.Model):
+  user = models.ForeignKey(User, models.DO_NOTHING)
+  chat = models.ForeignKey(Chat, models.DO_NOTHING)
+  message = models.ForeignKey(Message, models.DO_NOTHING)
+  form = models.CharField(max_length=50, null=False)
+  url = models.CharField(max_length=50, null=False) 
+
+class Member(models.Model):
+  user = models.ForeignKey(User, models.DO_NOTHING)
+  chat = models.ForeignKey(Chat, models.DO_NOTHING)
+  new_messages = models.CharField(max_length=30, null=False)
+  last_read_message_id = models.OneToOneField(Message, models.DO_NOTHING)
+  
--- messenger/users/models.py
+++ messenger/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=50, null=False)
+  nick = models.CharField(max_length=30, null=False)
+  avatar = models.CharField(max_length=100, null=False)
